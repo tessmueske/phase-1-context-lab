e516434a14bbaf29c94851f891c1ee639a94761f
Tess <tess.mueske@gmail.com>
2024-03-14 21:59:02 +0200

    complete lab

--- index.js
+++ index.js
@@ -21,3 +21,78 @@ const allWagesFor = function () {
     return payable
 }
 
+function createEmployeeRecord(employeeInfo){
+    return {
+      firstName: employeeInfo[0],
+      familyName: employeeInfo[1],
+      title: employeeInfo[2],
+      payPerHour: employeeInfo[3],
+      timeInEvents: [],
+      timeOutEvents: []
+    };
+  }
+
+  function createEmployeeRecords(arrayOfEmployeeRecords){
+    let employeeRecords = [];
+    arrayOfEmployeeRecords.forEach(function(employeeInfo) {
+      let oneEmployeeRecord = createEmployeeRecord(employeeInfo);
+      employeeRecords.push(oneEmployeeRecord);
+    });
+    return employeeRecords;
+  }
+
+function createTimeInEvent(dateStamp){
+    let hour = parseInt(dateStamp.substring(11, 15));
+    let date = dateStamp.substring(0, 10);
+    let timeInEvent = {
+      type: "TimeIn",
+      hour: hour,
+      date: date
+    };
+    this.timeInEvents.push(timeInEvent);
+    return this
+  }
+  
+  function createTimeOutEvent(dateStamp){
+    let hour = parseInt(dateStamp.substring(11, 15))
+    let date = dateStamp.substring(0, 4) + '-' + dateStamp.substring(5, 7) + '-' + dateStamp.substring(8, 10);
+    let timeOutEvent = {
+      type: "TimeOut",
+      hour: hour,
+      date: date
+    }
+    this.timeOutEvents.push(timeOutEvent);
+    return this
+  }
+  
+  function hoursWorkedOnDate(dateStamp){
+    let date = dateStamp.substring(0, 4) + '-' + dateStamp.substring(5, 7) + '-' + dateStamp.substring(8, 10);
+    let timeInEvent = this.timeInEvents.find(event => event.date === date);
+    let timeOutEvent = this.timeOutEvents.find(event => event.date === date);
+    let hoursWorked = (timeOutEvent.hour - timeInEvent.hour) / 100;
+    return hoursWorked
+  }
+  
+  function wagesEarnedOnDate(dateStamp){
+    let payRate = this.payPerHour;
+    let wagesOwedOnDate = hoursWorkedOnDate.call(this, dateStamp) * payRate;
+    return wagesOwedOnDate
+  }
+
+  function findEmployeeByFirstName(srcArray, firstName){
+    for (let i = 0; i < srcArray.length; i++) {
+      if (srcArray[i].firstName === firstName) {
+        return srcArray[i];
+      }
+  }
+  return undefined;
+}
+
+  function calculatePayroll(employeeRecords) {
+    let totalPayroll = 0;
+    employeeRecords.forEach(function(employee) {
+        const wagesForEmployee = allWagesFor.call(employee);
+        totalPayroll += wagesForEmployee;
+    });
+    return totalPayroll;
+}
